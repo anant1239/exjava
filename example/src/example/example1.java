de3c44e6e87e1624cf56c8073c7290dd31811bcc
anant1239 <anantdubey@gmail.com>
2022-08-04 11:15:05 +0530

    Example

---
+++ example/src/example/example1.java
@@ -0,0 +1,20 @@
+package example;
+
+public class example1 {
+	
+protected int deptno;
+protected String depName;
+
+	void assign(int a, String b)
+	{
+		deptno=a;
+		depName=b;
+	}
+	void display()
+	{
+		System.out.println(deptno);
+		System.out.println(depName);
+	}
+	
+
+}
---
+++ example/src/example/example2.java
@@ -0,0 +1,11 @@
+package example;
+
+public class example2 extends example1
+{
+	public static void main(String[] args) {
+		example1 ex=new example1();
+		ex.assign(10, "Accenture");
+		ex.display();
+	}
+}
+
